58fccac55eb5ebb56cb41137c74b843549e52738
abeltsew <35835135+abeltsew@users.noreply.github.com>
2023-03-24 13:55:12 +0300

    Add "Clear completed" tasks

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import './style.css';
-import { add, removeTask } from './addRemove.js';
+import { add, removeTask, clearCompleted } from './addRemove.js';
 import todoList from './TodoList.js';
 
 let currentItem = '';
@@ -27,70 +27,78 @@ input.addEventListener('keyup', (e) => {
 
 const todos = document.querySelector('.todos');
 
-todoList
-  .sort((a, b) => a.index - b.index)
-  .forEach((todo) => {
-    clear.remove();
-    const li = document.createElement('li');
-    li.classList.add('list-field');
-    li.id = todo.index;
-    const div = document.createElement('div');
-    div.classList.add('list-label');
-
-    const inputCheck = document.createElement('input');
-    inputCheck.type = 'checkbox';
-    inputCheck.checked = !!todo.completed;
-    inputCheck.addEventListener('change', () => {
-      const update = todoList.map((todo) => {
-        if (todo.index === Number(li.id)) {
-          todo.completed = inputCheck.checked;
+export const renderList = (list) => {
+  const oldList = document.querySelectorAll('.task-item');
+  oldList.forEach((ti) => ti.remove());
+  list
+    .sort((a, b) => a.index - b.index)
+    .forEach((todo) => {
+      clear.remove();
+      const li = document.createElement('li');
+      li.classList.add('list-field');
+      li.classList.add('task-item');
+      li.id = todo.index;
+      const div = document.createElement('div');
+      div.classList.add('list-label');
+      const inputCheck = document.createElement('input');
+      inputCheck.type = 'checkbox';
+      inputCheck.checked = !!todo.completed;
+      inputCheck.addEventListener('change', () => {
+        const update = todoList.map((todo) => {
+          if (todo.index === Number(li.id)) {
+            todo.completed = inputCheck.checked;
+            return todo;
+          }
           return todo;
-        }
-        return todo;
+        });
+        todoList.splice(0, todoList.length, ...update);
+        localStorage.setItem('todoList', JSON.stringify(todoList));
       });
-      todoList.splice(0, todoList.length, ...update);
-      localStorage.setItem('todoList', JSON.stringify(todoList));
-    });
 
-    const inputField = document.createElement('input');
-    inputField.classList.add('list-input');
-    inputField.value = todo.description;
+      const inputField = document.createElement('input');
+      inputField.classList.add('list-input');
+      inputField.value = todo.description;
 
-    div.appendChild(inputCheck);
-    div.appendChild(inputField);
-    li.appendChild(div);
-    const icon = document.createElement('span');
-    icon.innerHTML = "<i class='fa-solid fa-ellipsis-vertical'></i>";
-    li.appendChild(icon);
+      div.appendChild(inputCheck);
+      div.appendChild(inputField);
+      li.appendChild(div);
+      const icon = document.createElement('span');
+      icon.innerHTML = "<i class='fa-solid fa-ellipsis-vertical'></i>";
+      li.appendChild(icon);
 
-    icon.addEventListener(
-      'click',
-      () => icon.getAttribute('icon') === 'delete' && removeTask(currentItem),
-    );
+      icon.addEventListener(
+        'click',
+        () => icon.getAttribute('icon') === 'delete' && removeTask(currentItem)
+      );
 
-    li.addEventListener('click', () => {
-      currentItem = li.id;
-      const allInput = document.querySelectorAll('span');
-      allInput.forEach((i) => {
-        i.innerHTML = "<i class='fa-solid fa-ellipsis-vertical'></i>";
-        i.setAttribute('icon', 'move');
-        i.style.color = 'black';
+      li.addEventListener('click', () => {
+        currentItem = li.id;
+        const allInput = document.querySelectorAll('span');
+        allInput.forEach((i) => {
+          i.innerHTML = "<i class='fa-solid fa-ellipsis-vertical'></i>";
+          i.setAttribute('icon', 'move');
+          i.style.color = 'black';
+        });
+        icon.innerHTML = "<i class='fa-solid fa-trash-can'></i>";
+        icon.setAttribute('icon', 'delete');
+        icon.style.color = 'red';
       });
-      icon.innerHTML = "<i class='fa-solid fa-trash-can'></i>";
-      icon.setAttribute('icon', 'delete');
-      icon.style.color = 'red';
-    });
-    todos.appendChild(li);
-    todos.append(clear);
-    li.addEventListener('keyup', (e) => {
-      const update = todoList.map((todo) => {
-        if (todo.index === Number(li.id)) {
-          todo.description = e.target.value;
+      todos.appendChild(li);
+      todos.append(clear);
+      li.addEventListener('keyup', (e) => {
+        const update = todoList.map((todo) => {
+          if (todo.index === Number(li.id)) {
+            todo.description = e.target.value;
+            return todo;
+          }
           return todo;
-        }
-        return todo;
+        });
+        todoList.splice(0, todoList.length, ...update);
+        localStorage.setItem('todoList', JSON.stringify(todoList));
       });
-      todoList.splice(0, todoList.length, ...update);
-      localStorage.setItem('todoList', JSON.stringify(todoList));
     });
-  });
+};
+
+renderList(todoList);
+
+clear.addEventListener('click', () => clearCompleted());
